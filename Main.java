afc5e40233663d28e7828b1c0fe681b0c7fa29d6
Enzo-Oliver <152569686+Enzo-Oliver@users.noreply.github.com>
2026-08-18 12:30:25 +0000

    Classe main

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        
+        // Exemplo simples: soma de dois números
+        int a = 5;
+        int b = 7;
+        int soma = a + b;
+        System.out.println("A soma de " + a + " + " + b + " é: " + soma);
+    }
+} 
---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        
+        int a = 5;
+        int b = 7;
+        int soma = a + b;
+        System.out.println("A soma de " + a + " + " + b + " é: " + soma);
+    }
+} 
